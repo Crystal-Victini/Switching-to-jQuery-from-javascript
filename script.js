dd8e2ec3b7e67bd5a2f204a1dc81a239a4cf698c
Amy Bowen <amybowen2@gmail.com>
2022-06-22 20:45:48 -0400

    All changes to the file, tried committing before but forgot to save changes on vsc so it didn't go through

---
+++ script.js
@@ -0,0 +1,8 @@
+  $(document).ready( function(){
+    $("#theArea").hover ( function(){
+        $(this).css({"background-color": "#008000"})
+    },
+    function(){
+        $(this).css({"background-color": "#953674"})
+    })
+  });
